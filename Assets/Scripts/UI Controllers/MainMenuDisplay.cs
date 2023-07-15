8e76977ecd4ee368d39b2535b5d5a78ffd5bfa8c
Dannan Vazquez <dannanvazquez@gmail.com>
2023-07-14 20:58:05 -0700

    Initial Unity Project
    Simple Unity project using Netcode. Only features creating a server and joining the server.

---
+++ Assets/Scripts/UI Controllers/MainMenuDisplay.cs
@@ -0,0 +1,14 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuDisplay : MonoBehaviour {
+    public void StartServer(string gameSceneName) {
+        NetworkManager.Singleton.StartServer();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+    }
+
+    public void StartClient() {
+        NetworkManager.Singleton.StartClient();
+    }
+}
