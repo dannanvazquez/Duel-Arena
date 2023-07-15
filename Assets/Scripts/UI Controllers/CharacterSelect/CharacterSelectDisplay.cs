038c7514e4a7ddfada7f51ed1262d96fcea2604f
Dannan Vazquez <dannanvazquez@gmail.com>
2023-07-15 02:19:45 -0700

    Simple Character Select
    Instead of going into the game, immediately go into a character select scene where players can choose their character. Not much more than that yet.

---
+++ Assets/Scripts/UI Controllers/CharacterSelect/CharacterSelectDisplay.cs
@@ -0,0 +1,98 @@
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+public class CharacterSelectDisplay : NetworkBehaviour {
+    [SerializeField] private CharacterDatabase characterDatabase;
+    [SerializeField] private Transform charactersHolder;
+    [SerializeField] private CharacterSelectButton selectButtonPrefab;
+    [SerializeField] private PlayerCard[] playerCards;
+    [SerializeField] private GameObject characterInfoPanel;
+    [SerializeField] private TMP_Text characterNameText;
+
+    private NetworkList<CharacterSelectState> players;
+
+    private void Awake() {
+        players = new NetworkList<CharacterSelectState>();
+    }
+
+    public override void OnNetworkSpawn() {
+        if (IsClient) {
+            Character[] allCharacters = characterDatabase.GetAllCharacters();
+
+            foreach (var character in allCharacters) {
+                var selectButtonInstance = Instantiate(selectButtonPrefab, charactersHolder);
+                selectButtonInstance.SetCharacter(this, character);
+            }
+
+            players.OnListChanged += HandlePlayersStateChanged;
+        }
+
+        if (IsServer) {
+            NetworkManager.Singleton.OnClientConnectedCallback += HandleClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback += HandleClientDisconnected;
+
+            foreach (NetworkClient client in NetworkManager.Singleton.ConnectedClientsList) {
+                HandleClientConnected(client.ClientId);
+            }
+        }
+    }
+
+    public override void OnNetworkDespawn() {
+        if (IsClient) {
+            players.OnListChanged -= HandlePlayersStateChanged;
+        }
+
+        if (IsServer) {
+            NetworkManager.Singleton.OnClientConnectedCallback -= HandleClientConnected;
+            NetworkManager.Singleton.OnClientDisconnectCallback -= HandleClientDisconnected;
+        }
+    }
+
+    private void HandleClientConnected(ulong clientId) {
+        players.Add(new CharacterSelectState(clientId));
+    }
+
+    private void HandleClientDisconnected(ulong clientId) {
+        /*for (int i = 0; i < players.Count; i++) {
+            if (players[i].ClientId == clientId) {
+                players.RemoveAt(i);
+                break;
+            }
+        }*/
+        foreach (var player in players) {
+            if (player.ClientId == clientId) {
+                players.Remove(player);
+                break;
+            }
+        }
+    }
+
+    public void SelectCharacter(Character character) {
+        characterNameText.text = character.DisplayName;
+
+        characterInfoPanel.SetActive(true);
+
+        SelectServerRpc(character.Id);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void SelectServerRpc(int characterId, ServerRpcParams serverRpcParams = default) {
+        for (int i = 0; i < players.Count; i++) {
+            if (players[i].ClientId == serverRpcParams.Receive.SenderClientId) {
+                players[i] = new CharacterSelectState(players[i].ClientId, characterId);
+                break;
+            }
+        }
+    }
+
+    private void HandlePlayersStateChanged(NetworkListEvent<CharacterSelectState> changeEvent) {
+        for (int i = 0; i < playerCards.Length; i++) {
+            if (players.Count > i) {
+                playerCards[i].UpdateDisplay(players[i]);
+            } else {
+                playerCards[i].DisableDisplay();
+            }
+        }
+    }
+}
--- Assets/Scripts/UI Controllers/MainMenuDisplay.cs
+++ Assets/Scripts/UI Controllers/MainMenuDisplay.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuDisplay : MonoBehaviour {
+    public void StartHost(string gameSceneName) {
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
+    }
+
     public void StartServer(string gameSceneName) {
         NetworkManager.Singleton.StartServer();
         NetworkManager.Singleton.SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
