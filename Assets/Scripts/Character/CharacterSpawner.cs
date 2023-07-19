741c3b203aabb2c8f743fce0f7a9586f2cfb5c0a
Dannan Vazquez <dannanvazquez@gmail.com>
2023-07-19 05:32:41 -0700

    MoveToClickPoint Characters
    Players can move characters where they click. Sends destination to the server and syncs with clients to prevent cheating.

--- Assets/Scripts/Character/CharacterSpawner.cs
+++ Assets/Scripts/Character/CharacterSpawner.cs
@@ -12,7 +12,7 @@ public class CharacterSpawner : NetworkBehaviour {
         foreach (var client in ServerManager.Instance.ClientData) {
             var character = characterDatabase.GetCharacterById(client.Value.characterId);
             if (character != null) {
-                var spawnPos = new Vector3(Random.Range(-3f, 3f), 0, Random.Range(-3f, 3f));
+                var spawnPos = new Vector3(0f, 0.5625f, 0f);
                 var characterInstance = Instantiate(character.GameplayPrefab, spawnPos, Quaternion.identity);
                 characterInstance.SpawnAsPlayerObject(client.Value.clientId);
             }
---
+++ Assets/Scripts/Character/MoveToClickPoint.cs
@@ -0,0 +1,47 @@
+using Unity.Burst.CompilerServices;
+using Unity.Netcode;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MoveToClickPoint : NetworkBehaviour {
+    [SerializeField] private Animator animator;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Start() {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+    }
+
+    private void Update() {
+        if (IsLocalPlayer) {
+            if (Input.GetMouseButton(1)) {
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 100)) {
+                    SetDestinationServerRpc(hit.point);
+                }
+            }
+        }
+
+        if (animator) {
+            animator.SetBool("IsRunning", navMeshAgent.velocity != Vector3.zero);
+        }
+    }
+
+    private void LateUpdate() {
+        if (navMeshAgent.velocity != Vector3.zero ) {
+            transform.rotation = Quaternion.LookRotation(navMeshAgent.velocity.normalized);
+        }
+    }
+
+    [ServerRpc]
+    private void SetDestinationServerRpc(Vector3 dest) {
+        SetDestinationClientRpc(dest);
+    }
+
+    [ClientRpc]
+    private void SetDestinationClientRpc(Vector3 dest) {
+        navMeshAgent.destination = dest;
+    }
+}
